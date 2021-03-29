cdaf8832206f45ad34152e17fa5bb58d980a0705
calmahn <acy824@naver.com>
2021-03-29 23:05:23 +0900

    20210329

--- 03-1-02.py
+++ 03-1-02.py
@@ -1,6 +1,5 @@
-money = True
-
-if money:
+money = 2000
+if money >= 3000:
     print("택시를 타고 가라")
 else:
     print("걸어 가라")
--- 03-1-03.py
+++ 03-1-03.py
@@ -1,6 +1,6 @@
-money = True
-
-if money:
+money = 2000
+card = True
+if money >= 3000 or card:
     print("택시를 타고 가라")
 else:
     print("걸어 가라")
